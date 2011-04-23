bfc5b19b618c1a0f85836e03bf5c9ee02a1170ff
Konrad Malawski <konrad.malawski@project13.pl>
2011-04-23 15:33:57 +0200

    marker interface for required fields (based on Kanbanery API documentation)

--- src/main/java/pl/project13/janbanery/resources/additions/Required.java
+++ src/main/java/pl/project13/janbanery/resources/additions/Required.java
@@ -1,7 +1,19 @@
 package pl.project13.janbanery.resources.additions;
 
+import com.google.common.annotations.GwtCompatible;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
+ * Just a marker annotation to carry the information from the Kanbanery API description right into our resource classes.
+ * A field with this annotation should always be set when working with such entities.
+ *
  * @author Konrad Malawski
  */
+@Documented
+@GwtCompatible
+@Retention(RetentionPolicy.SOURCE)
 public @interface Required {
 }
